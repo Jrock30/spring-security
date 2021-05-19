783a8c6f42c9a865f5808969fb89c9e2b31b855d
Jrock30 <jrg8989@knou.ac.kr>
2021-05-19 22:07:28 +0900

    feat: Spring SecurityConfig ADD - SecurityConfig 파일 추가 - WebSecurityConfigurerAdapter 구현체 - 인증 관련 설정

---
+++ src/main/java/com/jrock/basicsecurity/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.jrock.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * WebSecurityConfigurerAdapter 를 상속 받아 기본 기능들 재정의 한다.
+ */
+@Configuration
+@EnableWebSecurity // 웹 보안 활성화 어노테이션
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated(); // 어떤 요청에도 인증을 받도록
+
+        http
+                .formLogin()                            // 인증 폼 로그인 방식
+//                .loginPage("/loginPage")                // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/")                 // 로그인 성공 후 이동 페이지, 우선순위가 맨 마지막 ( successHandler 보다 뒤 ), 우선순위 당기려면 두번째 인자에 true 주면 됨 기본은 false
+                .failureUrl("/home")                    // 로그인 실패 후 이동 페이지
+                .usernameParameter("userId")            // 아이디 파라미터명 설정
+                .passwordParameter("password")          // 패스워드 파라미터명 설정
+                .loginProcessingUrl("/login_proc")      // 로그인 Form Action Url
+                .successHandler(new AuthenticationSuccessHandler() {    // 로그인 성공 후 핸들러
+                    @Override // 익명 클래스 생성
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication.getName());
+                        httpServletResponse.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {    // 로그인 실패 후 핸들러
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        System.out.println("exception : " + e.getMessage());
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })
+        .permitAll() // 위의 페이지에는 모두 접근이 가능하도록 설정 /login (default), /loginPage (주석처리 부분 패스)
+        ;
+
+    }
+}
+
